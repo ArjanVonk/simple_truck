16009419f67c9a4f9f517cda64084ccd89eca315
RJ <arjan.vonk@outlook.com>
2023-10-18 12:40:11 +0200

    feat: add simple listener

--- simple_truck/simple_truck_node.py
+++ simple_truck/simple_truck_node.py
@@ -9,13 +9,13 @@ class SimpleTruckNode(Node):
     def __init__(self):
         super().__init__('simple_truck_node')
         self.publisher_ = self.create_publisher(String, 'simple_truck_status', 10)
+        self.subscription = self.create_subscription(String, 'simple_truck_listener', self.listener_callback, 10)
 
         self.truck = Truck()
         self.truck.start_truck()
         self.truck.set_throttle_percentage(50)
-        timer_period = 0.5  # seconds
+        timer_period = 0.5
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
 
     def timer_callback(self):
         self.truck.drive()
@@ -23,6 +23,16 @@ class SimpleTruckNode(Node):
         msg.data = self.truck.get_status()
         self.publisher_.publish(msg)
         self.get_logger().info(f'{msg.data}')
+
+    def listener_callback(self, msg):
+        match msg.data:
+            case "start":
+                self.truck.start_truck()
+            case "stop": 
+                self.truck.turn_off_truck()
+        
+
+
 
 
 def main(args=None):
